df93d0098dc087794247ae70b1c5db663e2b48aa
brtydse100 <brtydse100@gmail.com>
2022-09-20 18:40:01 +0300

    added the class i told you about

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -4,102 +4,70 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import utils.Module;
+import utils.Module1;
 import utils.Side;
 
 public class Chassis extends SubsystemBase {
+  private Module1 front_right, back_right, front_left, back_left;
 
-  private final Module front_right, back_right, front_left, back_left;
-  private final Translation2d front_left_location, front_right_location, back_left_location , back_right_location;
-  private final Module[] modules;
-  private final SwerveDriveKinematics kinematics;
-  private final SwerveDriveOdometry odometry;
-  private final PigeonIMU gyro;
-  private Pose2d robot_current_position;
+  Translation2d frontLeftLocation = new Translation2d(Constants.FRONT_LEFT_LOCATION_X, Constants.FRONT_LEFT_LOCATION_Y);
+  Translation2d frontRightLocation = new Translation2d(Constants.FRONT_RIGHT_LOCATION_X,
+      Constants.FRONT_RIGHT_LOCATION_Y);
+  Translation2d backLeftLocation = new Translation2d(Constants.BACK_LEFT_LOCATION_X, Constants.BACK_LEFT_LOCATION_Y);
+  Translation2d backRightLocation = new Translation2d(Constants.BACK_RIGHT_LOCATION_X, Constants.BACK_RIGHT_LOCATION_Y);
+
+  SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+      frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
   /** Creates a new Chassis. */
-  public Chassis() {
 
-    robot_current_position = new Pose2d(new Translation2d(Constants.STARTING_POSITION_X, Constants.STARTING_POSITION_Y), 
-        new Rotation2d(Constants.STARTING_ANGLE));
+  {
+    this.front_right = new Module1(Constants.FRONT_RIGHT_MOVE, Constants.FRONT_RIGHT_TURN, Constants.FRONT_RIGHT_CODER);
+    this.back_right = new Module1(Constants.BACK_RIGHT_MOVE, Constants.BACK_RIGHT_TURN, Constants.BACK_RIGHT_CODER);
+    this.front_left = new Module1(Constants.FRONT_LEFT_MOVE, Constants.FRONT_LEFT_TURN, Constants.FRONT_LEFT_CODER);
+    this.back_left = new Module1(Constants.BACK_LEFT_MOVE, Constants.BACK_LEFT_TURN, Constants.BACK_LEFT_CODER);
 
-    this.gyro = new PigeonIMU(Constants.GYRO);
-
-    //define moduls
-    this.front_right = new Module(Side.FRONT_RIGHT);
-    this.back_right = new Module(Side.BACK_RIGHT);
-    this.front_left = new Module(Side.FRONT_LEFT);
-    this.back_left = new Module(Side.BACK_LEFT);
-
-    //define moduls location
-    front_left_location = new Translation2d(Constants.FRONT_LEFT_LOCATION_X, Constants.FRONT_LEFT_LOCATION_Y);
-    front_right_location = new Translation2d(Constants.FRONT_RIGHT_LOCATION_X, Constants.FRONT_RIGHT_LOCATION_Y);
-    back_left_location = new Translation2d(Constants.BACK_LEFT_LOCATION_X, Constants.BACK_LEFT_LOCATION_Y);
-    back_right_location = new Translation2d(Constants.BACK_RIGHT_LOCATION_X, Constants.BACK_RIGHT_LOCATION_Y);
-
-    kinematics = new SwerveDriveKinematics(front_left_location, front_right_location, back_left_location, back_right_location);
-
-    odometry = new SwerveDriveOdometry(kinematics,getHeading(), robot_current_position);
-
-    modules = new Module[4];
-    modules[0] = front_left;
-    modules[1] = front_right;
-    modules[2] = back_left;
-    modules[3] = back_right;
-
-    configCoders();
-// i need you to find me the encouder of the turn motor ty :>
-  }
+  } // i need you to find me the encouder of the turn motor ty :>
 
   @Override
   public void periodic() {
-    //TODO
     // This method will be called once per scheduler run
-    
-    //robot_current_position =  odometry.update(();
   }
 
-  public SwerveModuleState[] getModuleStates(double vx, double vy, double radians_per_second) {
-    ChassisSpeeds chassisSpeeds = new ChassisSpeeds(vx, vy, radians_per_second);
-    SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(chassisSpeeds);
-    return moduleStates;
+  public SwerveModuleState[] angle_and_speed(double vx, double vy, double radians_per_second) { // read about it on the
+    ChassisSpeeds speeds = new ChassisSpeeds(vx, vy, radians_per_second);
+
+    SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
+    SwerveModuleState frontLeft = moduleStates[0];
+    SwerveModuleState frontRight = moduleStates[1];
+    SwerveModuleState backLeft = moduleStates[2];
+    SwerveModuleState backRight = moduleStates[3];
+    return null;
   }
 
-  public void configCoders() {
-    for (Module m : modules) {
-      m.getCanCoder().setPositionToAbsolute();
-    }
+  public void turn_optimize(SwerveModuleState frontLeft, SwerveModuleState frontRight, SwerveModuleState backLeft,
+      SwerveModuleState backRight) {
+    var frontLeftOptimized = SwerveModuleState.optimize(frontLeft,
+        new Rotation2d(frontLeft.getDistance())); // i dont know how to make the oprimaze
+
+    var frontRightOptimized = SwerveModuleState.optimize(frontLeft,
+        new Rotation2d(m_turningEncoder.getDistance()));
+
+    var backLeftOptimized = SwerveModuleState.optimize(frontLeft,
+        new Rotation2d(m_turningEncoder.getDistance()));
+
+    var backRightOptimized = SwerveModuleState.optimize(frontLeft,
+        new Rotation2d(m_turningEncoder.getDistance()));
+
   }
-
-  public Rotation2d getHeading(){
-    return  Rotation2d.fromDegrees(-gyro.getFusedHeading());
-  }
-
-  // public void turn_optimize(SwerveModuleState frontLeft, SwerveModuleState
-  // frontRight, SwerveModuleState backLeft,
-  // SwerveModuleState backRight) {
-  // var frontLeftOptimized = SwerveModuleState.optimize(frontLeft.,
-  // new Rotation2d(constam_turningEncoder.getDistance()));
-
-  // var frontRightOptimized = SwerveModuleState.optimize(frontLeft,
-  // new Rotation2d(m_turningEncoder.getDistance()));
-
-  // var backLeftOptimized = SwerveModuleState.optimize(frontLeft,
-  // new Rotation2d(m_turningEncoder.getDistance()));
-
-  // var backRightOptimized = SwerveModuleState.optimize(frontLeft,
-  // new Rotation2d(m_turningEncoder.getDistance()));
-
-  // }
 
 }
